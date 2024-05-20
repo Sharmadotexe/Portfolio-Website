de3bde4f58d689da2d72402f0755ab0c885634ce
sharmadotexe <rockvikas300@gmail.com>
2024-05-20 16:59:42 +0530

    Changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter } from "react-router-dom"
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+
+
+
 const App = ()=> {
   return (
-    <>
-      <div>
-        Personal Portfolio Project
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero />
+        </div>
       </div>
-    </>
+    </BrowserRouter>
   )
 }
 
